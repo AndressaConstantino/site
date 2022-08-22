2dab236d5c8ca4f49693a2cb804bdc13cf031a01
Andressa Constantino <90932756+AndressaConstantino@users.noreply.github.com>
2022-08-21 23:43:11 -0300

    Add files via upload
    primeiro esboço do site

---
+++ header.php
@@ -0,0 +1,13 @@
+<?
+
+?>
+<head>
+    <link href="style1.css" rel="stylesheet" type="text/css">
+    <meta charset="utf-8">
+</head>
+<div class="navbar">
+    <img src="https://media1.tenor.com/images/8aab6d056144aada70700c9182b09329/tenor.gif?itemid=16799196" width="5%">
+    <a href="index.php">Início</a>
+    <a href="news.php">Notícias</a>
+    <a href="contact.php" class="right">Contatos</a>
+</div>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?
+
+?>
+
+
+<!doctype html>
+<html lang="pt">
+
+<html>
+<head>
+    <link href="style1.css" rel="stylesheet" type="text/css">
+    <meta charset="utf-8">
+    <title>Home - Andressa</title>
+</head>
+<body style="height: 1000px">  
+    <?php include("header.php");?>
+    <center>
+        <h1>Andressa Constantino</h1><br>
+        <h2>Desenvolvedora de Sistemas</h2>
+        <h3>Seja Bem vindo(a) ao meu Portifólio</h3>
+        <div class="painel">
+            <img src="https://media1.tenor.com/images/8aab6d056144aada70700c9182b09329/tenor.gif?itemid=16799196">
+        </div>
+    </Center>
+</body>
+</html>
